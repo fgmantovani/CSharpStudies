cdcf8652562e67b4d4743f719f7e0ca0524f8402
fgmantovani <fg.mantovani@gmail.com>
2020-08-21 11:45:57 -0300

    Section 9 done

---
+++ S09_T03_Path/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S09_T03_Path
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Projects\CSharpFundamentals\HelloWorld\HelloWorld.sln";
+
+            var dotIndex = path.IndexOf('.');
+            var extension = path.Substring(dotIndex);
+
+            Console.WriteLine(Path.GetExtension(path));
+            Console.WriteLine(Path.GetFileName(path));
+            Console.WriteLine(Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine(Path.GetDirectoryName(path));
+        }
+    }
+}
---
+++ S09_T04_Exercises/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S09_T04_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // No example of it.
+
+        }
+    }
+}
